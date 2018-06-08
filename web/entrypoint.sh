0ee1a0d90706335fab113ec5c97207a4d391477a
Jc Din <jcdin@channelfix.com>
2018-06-08 12:32:30 +0800

    create docker compose for nginx and web setup, move djopentok to web dir for web docker

--- web/entrypoint.sh
+++ web/entrypoint.sh
@@ -2,5 +2,7 @@
 
 echo migrate django project
 python manage.py migrate
+echo collectstatic django project
+python manage.py collectstatic --noinput;
 echo Starting Gunicorn Server
 gunicorn -w 3 -b 0.0.0.0:${WEB_SERVER_PORT} djopentok.wsgi
